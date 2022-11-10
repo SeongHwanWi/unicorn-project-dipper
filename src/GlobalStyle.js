9cba126d2f80bd281784fe434d081f19f4cc0212
b_oxygen <rhdtkqkr@gmail.com>
2022-11-10 17:28:46 +0900

    :art: make titleText reactive

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -31,6 +31,12 @@ const GlobalStyle = createGlobalStyle`
     font-weight: 300;
     font-style: normal;
 }
+@font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+}
     `;
 
 export default GlobalStyle;
